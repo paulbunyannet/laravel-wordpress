37706a3327f4b0964503f7b8d0f4d4c2a3697133
Garrett Johnson <garrettj@paulbunyan.net>
2016-12-28 13:32:19 -0600

    Break into http and php-fpm packages
    Provides better separation and error handling

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ chmod -R 755 /var/www/html/storage/framework
 
 echo "Start of Composer Install"
 yarn install --no-bin-links
-composer install
+composer install --prefer-dist
 echo "End of Composer Install"
 
-apache2-foreground
+php-fpm
